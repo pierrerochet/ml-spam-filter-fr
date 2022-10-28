f6b97b84348df50c58dff5d348c53d483d9a1d25
pierrerochet <pierre-rochet@outlook.fr>
2022-10-28 22:49:23 +0200

    test: add tests

---
+++ app/test_main.py
@@ -0,0 +1,22 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
+        yield c
+
+
+def test_detect_spam(client):
+    json = {"text": "Vous avez gagné un cadeau, cliquez ici pour le recevoir !"}
+    response = client.post("/verify", json=json).json()
+    assert response["is_spam"] == True
+
+
+def test_detect_ham(client):
+    json = {"text": "On se retrouve à 14h devant chez moi"}
+    response = client.post("/verify", json=json).json()
+    assert response["is_spam"] == False
